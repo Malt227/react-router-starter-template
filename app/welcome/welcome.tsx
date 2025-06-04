63e83b331c79816416378d9f09ec4733473d1afe
Malt227 <65109623+Malt227@users.noreply.github.com>
2025-06-04 15:00:46 +0100

    Update welcome.tsx

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -68,8 +68,7 @@ const resources = [
     ),
   },
   {
-    href: "https://rmx.as/discord",
-    text: "Join Discord",
+    text: "Email: hello@fencefort.co.uk",
     icon: (
       <svg
         xmlns="http://www.w3.org/2000/svg"
